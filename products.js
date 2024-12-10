ae7bd1902690d3a3e0a246842b1b4a8eac38f7a3
Optsnipez <visheshmundra@gmail.com>
2024-12-10 21:43:14 +0400

    editing product.js file

--- products.js
+++ products.js
@@ -1,92 +1,92 @@
 let products = [
-    {
-        "id": 1001,
-        "name": "Ethical Hacking",
-        "description": "An interesting course on various hacking methods",
-        image: "images/ethicalhacking.jpeg",
-        "price": 30.00,
-        "availableInventory": 10,
-        "rating": 2
-    },
-    {
-        "id": 1002,
-        "name": "Web Development",
-        "description": "Learn the fundamentals of HTML, CSS, and JavaScript",
-        image: "images/webdevlopment.jpeg",
-        "price": 45.00,
-        "availableInventory": 15,
-        "rating": 4
-    },
-    {
-        "id": 1003,
-        "name": "Data Science",
-        "description": "An in-depth course on data analysis and machine learning",
-        image: "images/datascience.jpeg",
-        "price": 60.00,
-        "availableInventory": 8,
-        "rating": 5
-    },
-    {
-        "id": 1004,
-        "name": "Graphic Design",
-        "description": "Master the art of graphic design with Photoshop and Illustrator",
-        image: "images/design.jpeg",
-        "price": 35.00,
-        "availableInventory": 12,
-        "rating": 3
-    },
-    {
-        "id": 1005,
-        "name": "Cybersecurity",
-        "description": "Understand the core concepts of keeping systems secure",
-        image: "images/security.jpeg",
-        "price": 50.00,
-        "availableInventory": 5,
-        "rating": 4
-    },
-    {
-        "id": 1006,
-        "name": "Digital Marketing",
-        "description": "A comprehensive guide to marketing in the digital age",
-        image: "images/marketing.jpeg",
-        "price": 40.00,
-        "availableInventory": 20,
-        "rating": 5
-    },
-    {
-        "id": 1007,
-        "name": "Mobile App Development",
-        "description": "Learn to create mobile applications for Android and iOS",
-        image: "images/devlopment.jpeg",
-        "price": 55.00,
-        "availableInventory": 7,
-        "rating": 4
-    },
-    {
-        "id": 1008,
-        "name": "Artificial Intelligence",
-        "description": "Dive deep into AI techniques and applications",
-        image: "images/ai.jpeg",
-        "price": 75.00,
-        "availableInventory": 6,
-        "rating": 5
-    },
-    {
-        "id": 1009,
-        "name": "Project Management",
-        "description": "Learn project management principles and best practices",
-        image: "images/project.jpeg",
-        "price": 40.00,
-        "availableInventory": 18,
-        "rating": 3
-    },
-    {
-        "id": 1010,
-        "name": "Cloud Computing",
-        "description": "Understand cloud platforms and services like AWS and Azure",
-        image: "images/cloud.jpeg",
-        "price": 65.00,
-        "availableInventory": 9,
-        "rating": 4
-    }
+    // {
+    //     "id": 1001,
+    //     "name": "Ethical Hacking",
+    //     "description": "An interesting course on various hacking methods",
+    //     image: "images/ethicalhacking.jpeg",
+    //     "price": 30.00,
+    //     "availableInventory": 5,
+    //     "rating": 2
+    // },
+    // {
+    //     "id": 1002,
+    //     "name": "Web Development",
+    //     "description": "Learn the fundamentals of HTML, CSS, and JavaScript",
+    //     image: "images/webdevlopment.jpeg",
+    //     "price": 45.00,
+    //     "availableInventory": 5,
+    //     "rating": 4
+    // },
+    // {
+    //     "id": 1003,
+    //     "name": "Data Science",
+    //     "description": "An in-depth course on data analysis and machine learning",
+    //     image: "images/datascience.jpeg",
+    //     "price": 60.00,
+    //     "availableInventory": 5,
+    //     "rating": 5
+    // },
+    // {
+    //     "id": 1004,
+    //     "name": "Graphic Design",
+    //     "description": "Master the art of graphic design with Photoshop and Illustrator",
+    //     image: "images/design.jpeg",
+    //     "price": 35.00,
+    //     "availableInventory": 5,
+    //     "rating": 3
+    // },
+    // {
+    //     "id": 1005,
+    //     "name": "Cybersecurity",
+    //     "description": "Understand the core concepts of keeping systems secure",
+    //     image: "images/security.jpeg",
+    //     "price": 50.00,
+    //     "availableInventory": 5,
+    //     "rating": 4
+    // },
+    // {
+    //     "id": 1006,
+    //     "name": "Digital Marketing",
+    //     "description": "A comprehensive guide to marketing in the digital age",
+    //     image: "images/marketing.jpeg",
+    //     "price": 40.00,
+    //     "availableInventory": 5,
+    //     "rating": 5
+    // },
+    // {
+    //     "id": 1007,
+    //     "name": "Mobile App Development",
+    //     "description": "Learn to create mobile applications for Android and iOS",
+    //     image: "images/devlopment.jpeg",
+    //     "price": 55.00,
+    //     "availableInventory": 5,
+    //     "rating": 4
+    // },
+    // {
+    //     "id": 1008,
+    //     "name": "Artificial Intelligence",
+    //     "description": "Dive deep into AI techniques and applications",
+    //     image: "images/ai.jpeg",
+    //     "price": 75.00,
+    //     "availableInventory": 5,
+    //     "rating": 5
+    // },
+    // {
+    //     "id": 1009,
+    //     "name": "Project Management",
+    //     "description": "Learn project management principles and best practices",
+    //     image: "images/project.jpeg",
+    //     "price": 40.00,
+    //     "availableInventory": 5,
+    //     "rating": 3
+    // },
+    // {
+    //     "id": 1010,
+    //     "name": "Cloud Computing",
+    //     "description": "Understand cloud platforms and services like AWS and Azure",
+    //     image: "images/cloud.jpeg",
+    //     "price": 65.00,
+    //     "availableInventory": 5,
+    //     "rating": 4
+    // }
 ];
